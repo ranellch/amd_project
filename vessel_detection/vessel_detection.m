e31dcbfc6305f719142dc9f248ce0990db70ff27
patnatha <patnatha@umich.edu>
2013-05-15 10:34:44 -0400

    Got some vessel detection working

--- vessel_detection/vessel_detection.m
+++ vessel_detection/vessel_detection.m
@@ -1,7 +1,31 @@
 function [data] = compare_maculas()
 	%Read in the original image
-	color = imread('1.jpg');
+	img = '1.jpg';
+	color = imread(img);
 	original = rgb2gray(color);
+	optic_x = 0;
+	optic_y = 0;
+	macula_x = 0;
+	macula_y = 0;
+
+	%Open the XML document and parse
+	xDoc= xmlread('1.xml');
+	xRoot = xDoc.getDocumentElement;
+
+	%Get the image element from the XML file
+	images = xRoot.getChildNodes;
+	image = images.getChildNodes;
+
+	%Parse out the values from XML file
+	optic_x = str2num(image.getElementsByTagName('optic_disk').item(0).getElementsByTagName('x').item(0).getTextContent);
+	optic_y = str2num(image.getElementsByTagName('optic_disk').item(0).getElementsByTagName('y').item(0).getTextContent);
+	macula_x = str2num(image.getElementsByTagName('macula').item(0).getElementsByTagName('x').item(0).getTextContent);
+	macula_y = str2num(image.getElementsByTagName('macula').item(0).getElementsByTagName('y').item(0).getTextContent);
+
+	%Get the distance between optic disk and macula 
+	radius = sqrt(power(optic_x - macula_x, 2) + power(optic_y - macula_y, 2)) * .8;
+
+	disp(strcat(img, ' - macula_x: ', int2str(macula_x), ' macula_y: ', int2str(macula_y), ' - radius: ', num2str(radius)));
 
 	%Run Gaussian filter
 	G_para = fspecial('gaussian',[5 5], 1.2);
@@ -11,18 +35,51 @@ function [data] = compare_maculas()
 	close_filter = imclose(g_filter, strel('square', 31));
 	tophat = imsubtract(close_filter, g_filter);
 
-	%Set either value to zero or to max
-	mean_val = mean(mean(tophat)) * 3;
-	for x = 1:size(tophat,1)
-		for y = 1:size(tophat,2)
-			pixel = tophat(x, y);
-			if(pixel < mean_val)
-				tophat(x, y) = 0;
+	%Get the mean value of the grayscale
+	mean_val = double(0);
+	count = 0;
+	for y = 1:size(tophat,1)
+		for x = 1:size(tophat,2)
+			mean_val = mean_val + double(tophat(y, x));
+			count = count + 1;
+		end
+	end
+	mean_val = mean_val / count;
+
+	%Calculate the standard deviation for the distribution of gray sacle values
+	variance = double(0);
+	for y = 1:size(tophat,1)
+		for x = 1:size(tophat,2)
+			variance = variance + power((mean_val - double(tophat(y, x))), 2);
+		end
+	end
+	stddev = sqrt(variance / count);
+
+	disp(strcat('mean: ', num2str(mean_val), ' stdev: ', num2str(stddev)));
+
+	threshold = mean_val + stddev;
+
+	%Threshold this badboy
+	for x = 1:size(tophat,2)
+		for y = 1:size(tophat,1)
+			%Calculate the distnace from the macula the current point 
+			distance = sqrt(power(double(x) - macula_x, 2) + power(double(y) - macula_y, 2));
+			
+			%If the distance is greater than the radius then it is outside the circle
+			if distance >= radius
+				pixel = tophat(y, x);
+				if(pixel < threshold)
+					tophat(y, x) = 0;
+				else
+					tophat(y, x) = 255;
+				end
 			else
-				tophat(x, y) = 255;
+				tophat(y, x) = 0;
 			end
 		end
 	end
+
+	%imwrite(tophat, 'test.jpg');
 
 	%Use open morphological filtering
 	openmorph = imopen(tophat, strel('square', 3));
