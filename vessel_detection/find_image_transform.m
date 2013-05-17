c095d11e096f59d41fff4dc11b5174699b78a431
patnatha <patnatha@umich.edu>
2013-05-17 11:49:25 -0400

    Made the transform a funcation and made main .m file that parses xml file and then runs on all three images

---
+++ vessel_detection/find_image_transform.m
@@ -0,0 +1,50 @@
+function [out] = find_transform()
+	img = '00036771';
+	
+	xDoc= xmlread('images.xml');
+	images = xDoc.getElementsByTagName('image');
+
+	images_to_compare = containers.Map('KeyType','char','ValueType','any');
+
+	%Loop on the image field in the images tag
+	for count = 1:images.getLength
+		image = images.item(count - 1);
+
+		%Get the attribute from the image tag
+		id = char(image.getAttribute('id'));
+		time = char(image.getAttribute('time')); 
+		path = char(image.getAttribute('path'));
+		if strcmpi(id, img) == 1
+			optic_x = char(image.getElementsByTagName('optic_disk').item(0).getElementsByTagName('x').item(0).getTextContent);
+			optic_y = char(image.getElementsByTagName('optic_disk').item(0).getElementsByTagName('y').item(0).getTextContent);
+			macula_x = char(image.getElementsByTagName('macula').item(0).getElementsByTagName('x').item(0).getTextContent);
+			macula_y = char(image.getElementsByTagName('macula').item(0).getElementsByTagName('y').item(0).getTextContent);
+			images_to_compare(time) = [path, ',', optic_x,',', optic_y,',', macula_x,',', macula_y];
+		end
+	end
+
+	%Run the transform on the images
+	for count = 1:length(images_to_compare)
+		key = int2str(count - 1);
+		info_array = images_to_compare(key);
+		disp(strcat('Image(', int2str(count), '):  ', info_array));
+		
+		splitit = regexp(info_array, '[,]', 'split');
+		file = char(splitit(1));
+		optic_x = str2num(char(splitit(2)));
+		optic_y = str2num(char(splitit(3)));
+		macula_x = str2num(char(splitit(4)));
+		macula_y = str2num(char(splitit(5)));
+		filename = vessel_detection(file, optic_x, optic_y, macula_x, macula_y);
+
+		images_to_compare(key) = filename;
+	end
+
+	%Get the offest of the images
+	base_img = images_to_compare('0');
+	for count = 2:length(images_to_compare)
+		next_img = char(images_to_compare(key));
+
+		
+	end
+end
--- vessel_detection/vessel_detection.m
+++ vessel_detection/vessel_detection.m
@@ -1,27 +1,11 @@
-function [data] = compare_maculas()
+function [outfile] = detect_vessels(img, optic_x, optic_y, macula_x, macula_y)
 	%Read in the original image
-	img = '1';
-	color = imread(strcat(img, '.jpg'));
+	img_val = regexp(img, '[.]', 'split');
+	img_name = char(img_val(1));
+	img_extension = char(img_val(2));
+	color = imread(img);
 	original = rgb2gray(color);
-	optic_x = 0;
-	optic_y = 0;
-	macula_x = 0;
-	macula_y = 0;
-
-	%Open the XML document and parse
-	xDoc= xmlread('1.xml');
-	xRoot = xDoc.getDocumentElement;
-
-	%Get the image element from the XML file
-	images = xRoot.getChildNodes;
-	image = images.getChildNodes;
-
-	%Parse out the values from XML file
-	optic_x = str2num(image.getElementsByTagName('optic_disk').item(0).getElementsByTagName('x').item(0).getTextContent);
-	optic_y = str2num(image.getElementsByTagName('optic_disk').item(0).getElementsByTagName('y').item(0).getTextContent);
-	macula_x = str2num(image.getElementsByTagName('macula').item(0).getElementsByTagName('x').item(0).getTextContent);
-	macula_y = str2num(image.getElementsByTagName('macula').item(0).getElementsByTagName('y').item(0).getTextContent);
-
+	
 	%Get the distance between optic disk and macula 
 	radius = sqrt(power(optic_x - macula_x, 2) + power(optic_y - macula_y, 2)) * .8;
 
@@ -84,9 +68,12 @@ function [data] = compare_maculas()
 
 	%Calculate the skeleton on the image
 	skel = bwmorph(openmorph, 'skel', Inf);
-	
+
 	%Save to disk
-	imwrite(skel, strcat('test_', img ,'.jpg'));
+	output = strcat('vd_', img_name ,'.jpg');
+	imwrite(skel, output);
+
+	outfile = output;
 end
 
 
