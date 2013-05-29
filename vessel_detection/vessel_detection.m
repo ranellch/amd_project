c011956569be37596e9e590c03c25c587bf8b0d3
patnatha <patnatha@umich.edu>
2013-05-29 18:30:09 -0400

    update to date

--- vessel_detection/vessel_detection.m
+++ vessel_detection/vessel_detection.m
@@ -1,12 +1,7 @@
-function [outfile] = vessel_detection(img, optic_x, optic_y, macula_x, macula_y, type, x1, y1, x2, y2)
-	%Read in the original image
-	img_val = regexp(img, '[.]', 'split');
-	img_name = char(img_val(1));
+function [outfile] = vessel_detection(img, optic_x, optic_y, macula_x, macula_y)
+	original = rgb2gray(img);
     
-    %Get the image and convert to graysacle
-	original = rgb2gray(imread(img));
-
-	%Get the distance between optic disk and macula 
+    %Get the distance between optic disk and macula 
 	distance = sqrt(power(optic_x - macula_x, 2) + power(optic_y - macula_y, 2));
 	a = distance * .7;
 	b = distance;
@@ -16,15 +11,15 @@ function [outfile] = vessel_detection(img, optic_x, optic_y, macula_x, macula_y,
 	g_filter = imfilter(original, G_para, 'same');
 
 	%Run closure and subtrace that from the original image
-	close_filter = imclose(g_filter, strel('square', 31));
-	tophat = imsubtract(close_filter, g_filter);
+	close_filter = imclose(g_filter, strel('square', 30));
+	out = imsubtract(close_filter, g_filter);
 
 	%Get the mean value of the grayscale
 	mean_val = double(0);
 	count = 0;
-	for y = 1:size(tophat,1)
-		for x = 1:size(tophat,2)
-			mean_val = mean_val + double(tophat(y, x));
+	for y = 1:size(out,1)
+		for x = 1:size(out,2)
+			mean_val = mean_val + double(out(y, x));
 			count = count + 1;
 		end
 	end
@@ -32,9 +27,9 @@ function [outfile] = vessel_detection(img, optic_x, optic_y, macula_x, macula_y,
 
 	%Calculate the standard deviation for the distribution of gray sacle values
 	variance = double(0);
-	for y = 1:size(tophat,1)
-		for x = 1:size(tophat,2)
-			variance = variance + power((mean_val - double(tophat(y, x))), 2);
+	for y = 1:size(out,1)
+		for x = 1:size(out,2)
+			variance = variance + power((mean_val - double(out(y, x))), 2);
 		end
 	end
 	stddev = sqrt(variance / count);
@@ -42,11 +37,9 @@ function [outfile] = vessel_detection(img, optic_x, optic_y, macula_x, macula_y,
     %From the mean and std dev calculate the threshold as one stddev
 	threshold = mean_val + stddev;
 
-    
-    
 	%Threshold this badboy
-	for x = 1:size(tophat,2)
-		for y = 1:size(tophat,1)
+	for x=1:size(out,2)
+		for y = 1:size(out,1)
             %Get the x and y distance from the macula that this point it
 			xdiff = abs(double(x) - macula_x);
 			ydiff = abs(double(y) - macula_y);
@@ -62,38 +55,23 @@ function [outfile] = vessel_detection(img, optic_x, optic_y, macula_x, macula_y,
 	
 			%If the distance is greater than the radius then it is outside the circle
 			if distance >= distance_ellipse
-				pixel = tophat(y, x);
+				pixel = out(y, x);
 				if(pixel < threshold)
-					tophat(y, x) = 0;
+					out(y, x) = 0;
 				else
-					tophat(y, x) = 255;
+					out(y, x) = 255;
 				end
 			else
-				tophat(y, x) = 0;
+				out(y, x) = 0;
 			end
 		end
-	end
-
+    end
+    
 	%Use open morphological filtering
-	openmorph = imopen(tophat, strel('square', 3));
+    out = bwmorph(out, 'close', 1);
+	out = bwmorph(out, 'bridge', Inf);
 
-	%Calculate the skeleton on the image
-	%skel = bwmorph(openmorph, 'skel', Inf);
-	%out = bwmorph(skel, 'bridge', Inf);
-	%out = bwmorph(out, 'thin', Inf);
-
-	%Crop the image
-	if strcmpi(type, 'square') == 1
-		diffx = x2 - x1;
-		diffy = y2 - y1;
-		cropped = imcrop(out, [x1, y1, diffx, diffy]);
-	end
-
-	%Save to disk
-	output = strcat('vd_', img_name ,'.jpg');
-	imwrite(cropped, output);
-
-	outfile = output;
+	outfile = out;
 end
 
 
