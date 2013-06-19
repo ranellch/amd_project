b482a3a6f5dbf24d6f304055d58744068ae477c3
Nathan Patel <nathanpatel@Nathans-MacBook-Air.local>
2013-06-19 10:19:41 -0400

    OK

--- vessel_detection/transform_it_vision.m
+++ vessel_detection/transform_it_vision.m
@@ -1,4 +1,4 @@
-function [theta, scale, translation, tform] = transform_it(pointsA, pointsB)
+function [theta, scale, translation, tform] = transform_it_vision(pointsA, pointsB)
     %Estimate the transform and set the parameters
     gte = vision.GeometricTransformEstimator;
     gte.Transform = 'Nonreflective similarity';
@@ -10,11 +10,10 @@ function [theta, scale, translation, tform] = transform_it(pointsA, pointsB)
     [tform] = step(gte, pointsB, pointsA);
     
     %Get the affine matrix
-    H = tform;
-    R = H(1:2,1:2);
+    R = tform(1:2,1:2);
     
     %Get the slace, theta, and translation of the affine matrix
     theta = mean([atan2(R(2),R(1)) atan2(-R(3),R(4))]);
     scale = mean(R([1 4])/cos(theta));
-    translation = H(3, 1:2);
+    translation = tform(3, 1:2);
 end
