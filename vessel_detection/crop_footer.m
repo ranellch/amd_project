58efa56439b54fb7b1b57196fa5a964795e17d13
Chris Ranella <ranellch@umich.edu>
2013-07-02 23:50:55 -0400

    tagging more automated

--- vessel_detection/crop_footer.m
+++ vessel_detection/crop_footer.m
@@ -2,7 +2,7 @@ function [ cropped_img ] = crop_footer( img )
 % Removes footer from grayscale FAF image matrices 
 
 i = round(size(img,1)/2); % start at middle
-while any(img(i:i+9,1:10))
+while any(img(i:i+9,:))
     if i+10 == size(img,1)
        cropped_img = img;
        return
