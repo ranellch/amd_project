f55505dc4a33fc023b1dd5b9a216a66ed2b943ad
patnatha <patnatha@umich.edu>
2013-07-04 13:28:31 -0400

    all fixed and up to date

--- vessel_detection/apply_transform.m
+++ vessel_detection/apply_transform.m
@@ -1,13 +1,10 @@
 function [img1out, img2out] = apply_transform(tform, imgbase, img2)
 	image1 = im2double(imgbase);
     image2 = im2double(img2);
+    
+	%resize the images
+    [img1out, image2] = match_sizing(image1, image2);
         
-    miny = min_axis(image1, image2, 1);
-    minx = min_axis(image1, image2, 2);
-    
-    img1out = imresize(image1, [minx, miny]);
-    image2 = imresize(image2, [minx, miny]);
-    
     agt = vision.GeometricTransformer;
     img2out = step(agt, image2, tform);
 end
