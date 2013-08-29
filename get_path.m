f192648aa8bc8e079e547b814d2ed6fb31a9a120
Nathan Patel <nathanpatel@unknown28cfe94abf63.gateway.2wire.net>
2013-08-28 22:56:39 -0400

    coolio

---
+++ get_path.m
@@ -0,0 +1,18 @@
+function [path] = get_path(pid, time)
+    %Add the location of the images
+    addpath(genpath('Test Set'));
+
+    %Parse XML document and find this pictures information
+    xDoc= xmlread('images.xml');
+    images = xDoc.getElementsByTagName('image');
+    
+	%Loop on the image field in the images tag
+    for count=1:images.getLength
+        image = images.item(count - 1);
+
+        if strcmp(pid, char(image.getAttribute('id'))) == 1 && ...
+           strcmp(time, char(image.getAttribute('time'))) == 1
+        	path = char(image.getAttribute('path'));
+        end
+    end
+end
--- vessel_detection/vessel_detection_curvlet.m
+++ vessel_detection/vessel_detection_curvlet.m
@@ -1,89 +1,100 @@
 function [out] = vessel_detection_curvlet(I)
-    I = rgb2gray(I);
-
+    pctg = 0.1;
+    
+    if length(size(I)) > 2
+        I = rgb2gray(I);
+    end
+    
     height = size(I,1);
     width = size(I,2);
     
-    border_clear = 10;
-    for y=1:height
-        for x=1:width
-            if((y <= border_clear || y >= (height - border_clear)) || ...
-               (x <= border_clear || x >= (width - border_clear)))
-                I(y, x) = 255;
-            end
+    C = fdct_wrapping(double(I), 0);
+    
+    % Get threshold value
+    cfs =[];
+    for s=1:length(C)
+      for w=1:length(C{s})
+        cfs = [cfs; abs(C{s}{w}(:))];
+      end
+    end
+    cfs = sort(cfs); cfs = cfs(end:-1:1);
+    nb = round(pctg*length(cfs));
+    cutoff = cfs(nb);
+    
+    % Set small coefficients to zero
+    for s=1:length(C)
+        for w=1:length(C{s})
+            C{s}{w} = C{s}{w} .* (abs(C{s}{w}) > cutoff);
         end
     end
     
-    %Get all the unique values in this image
-    unique_values_sorted = unique(I(:));
-    total_count_one_percent = height * width * 0.01;
+    %Rebuild the image using the modified coefficient values
+    Y = real(ifdct_wrapping(C,0));
     
-    %Calculate the number of values in the one percent
-    one_percent_image = zeros(height, width);
-    our_total_count = 0;
-    i = 1;
-    while our_total_count < total_count_one_percent && i <= length(unique_values_sorted)
-        for y=1:height
-            for x=1:width
-                if(I(y, x) == unique_values_sorted(i))
-                    one_percent_image(y, x) = 1;
-                    our_total_count = our_total_count + 1;
-                end
-            end
-        end
-        
-        i=i+1;
-    end
+    %Get a 5x5 structuring elements that are a line with 22.5 degrees of resolution
+    zeroline = [0,0,0,0,0;...
+                0,0,0,0,0;...
+                1,1,1,1,1;...
+                0,0,0,0,0;...
+                0,0,0,0,0;];
+    twentytwopointfive = [0,0,0,0,0;...
+                          0,0,0,0,1;...
+                          0,0,1,0,0;...
+                          1,0,0,0,0;...
+                          0,0,0,0,0;];
+    fortyfive = [0,0,0,0,1;...
+                 0,0,0,1,0;...
+                 0,0,1,0,0;...
+                 0,1,0,0,0;...
+                 1,0,0,0,0;];
+    sixtysevenpointfive = [0,0,0,1,0;...
+                           0,0,0,0,0;...
+                           0,0,1,0,0;...
+                           0,0,0,0,0;...
+                           0,1,0,0,0;];
+    vertline = [0,0,1,0,0;...
+                0,0,1,0,0;...
+                0,0,1,0,0;...
+                0,0,1,0,0;...
+                0,0,1,0,0;];
+     
+    %Combine all the images into a final image using each structuring element
+    final_img = zeros(height, width);
+    M = 8;
+    final_img = add_img(apply_morph(Y, zeroline), M, final_img);
+    final_img = add_img(apply_morph(Y, twentytwopointfive), M, final_img);
+    final_img = add_img(apply_morph(Y, fliplr(twentytwopointfive)), M, final_img);
+    final_img = add_img(apply_morph(Y, fortyfive), M, final_img);
+    final_img = add_img(apply_morph(Y, fliplr(fortyfive)), M, final_img);
+    final_img = add_img(apply_morph(Y, sixtysevenpointfive), M, final_img);
+    final_img = add_img(apply_morph(Y, fliplr(sixtysevenpointfive)), M, final_img);
+    final_img = add_img(apply_morph(Y, vertline), M, final_img);
     
-    zero_four_percent = round(height * width * 0.0004);
-    open_zero_four = bwareaopen(one_percent_image, zero_four_percent);
-    open_zero_four = bwmorph(open_zero_four, 'diag');
-    open_zero_four = bwmorph(open_zero_four, 'fill');
-    open_zero_four = imclose(open_zero_four, strel('disk',4));
-        
-    imshow(open_zero_four);
-    
-    %Find the centroid of each connected cluster
-    count = 0;
-    for y=1:height
-        for x=1:width
-            if(open_zero_four(y, x) == 1)
-                count = count + 1;
-            end
-        end
-    end
-    
-    X = zeros(count, 2);
-    index = 1;
-    for y=1:height
-        for x=1:width
-            if(open_zero_four(y, x) == 1)
-                X(index, 1) = x;
-                X(index, 2) = y;
-                index = index + 1;
-            end
-        end
-    end
-    
-    cc = bwconncomp(open_zero_four);
-    if(cc.NumObjects > 0)
-        [indx, ctrs] = kmeans(X, cc.NumObjects);
-        disp(ctrs);
-        disp(['The algorthim detected ', num2str(cc.NumObjects), ' clusters']);
-    end
-    
-    
-    
-    %Fix illumination
-    %u(x,y) = I(x,y) - k1*L(x,y) / k2*C(x,y)-
-    %L and C calculated using non uniform sampling grid shown in figure 2
-    %    At each point in the sampling grid a WxW window is use to calculate the 
-    %    the mean and sigma at each point window in the image.
-    %Then for each pixel in the image a Mahalanobis distance is calcuated to check
-    %    to see if a pixel belongs to the background or not
-    
-
-   
+    figure(2);
+    subplot(1,2,1); colormap gray; imagesc(real(I)); axis('image'); title('original image');
+    subplot(1,2,2); colormap gray; imagesc(real(Y)); axis('image'); title('partial reconstruction');
 end
 
+function [finalimg] = add_img(inputimg, M, finalimg)
+    if size(inputimg, 1) == size(finalimg, 1) && ...
+       size(inputimg, 2) == size(finalimg, 2)
+        for y=1:size(inputimg, 1)
+            for x=1:size(inputimg, 2)
+                finalimg(y, x) = finalimg(y, x) + (inputimg(y, x) / M);
+            end
+        end
+    else
+        disp('Incorrect SIZE');
+    end
+end
+
+function [out] = apply_morph(img, strelement)
+    newimg = imclose(img, strelement);
+    newimg = imopen(newimg, strelement);
+    
+    newimg1 = imdilate(newimg, strelement);
+    newimg2 = imerode(newimg, strelement);
+    
+    out = imsubtract(newimg1, newimg2);
+end
 
