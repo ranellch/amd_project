9d319759b11fde71c33118ccb495b9b7a83e40de
Nathan Patel <nathanpatel@Nathans-MacBook-Air.local>
2013-08-29 14:21:38 -0400

    Working on curvlet functionality for vessel detection

--- vessel_detection/vessel_detection_curvlet.m
+++ vessel_detection/vessel_detection_curvlet.m
@@ -28,8 +28,10 @@ function [out] = vessel_detection_curvlet(I)
         end
     end
     
+    C = modify_coefficients(C, I);
+    
     %Rebuild the image using the modified coefficient values
-    Y = real(ifdct_wrapping(C,0));
+    Y = real(ifdct_wrapping(C));
     
     %Get a 5x5 structuring elements that are a line with 22.5 degrees of resolution
     zeroline = [0,0,0,0,0;...
@@ -70,9 +72,84 @@ function [out] = vessel_detection_curvlet(I)
     final_img = add_img(apply_morph(Y, fliplr(sixtysevenpointfive)), M, final_img);
     final_img = add_img(apply_morph(Y, vertline), M, final_img);
     
+    figure(1);
+    imshow(final_img);
+    
     figure(2);
     subplot(1,2,1); colormap gray; imagesc(real(I)); axis('image'); title('original image');
     subplot(1,2,2); colormap gray; imagesc(real(Y)); axis('image'); title('partial reconstruction');
+end
+
+function [newCoeff] = modify_coefficients(C, img)
+    %Estimate the noise image standard deviation
+    sigma = img_stddev(img);
+    
+    %Loop through each sub-band
+    number_of_bands = length(C);
+    for j=1:number_of_bands
+        %Each sub-band can have one or more matricies of coefficients
+        number_of_coefficients = length(C{1, j});
+        for k=1:number_of_coefficients
+        	CoeffMatrix = C{1, j}{1, k};
+            process_subband_matrix(CoeffMatrix, sigma);
+        end
+    end
+end
+
+function [sigma] = img_stddev(img)
+    %This method was developed using the following paper
+    % WAVELET IMAGE DE-NOISING METHOD BASED ON NOISE STANDARD DEVIATION ESTIMATION
+    M = [ 1,-2, 1;
+         -2, 4,-2;
+          1,-2, 1];    
+    convolve = conv2(img, M);
+    convoluve_abs = abs(convolve);
+    summation = sum(convoluve_abs(:));
+    
+    k = size(img, 2);
+    l = size(img, 1);
+    sigma = sqrt((pi / 2) * (1 / (6 * (k - 2) * (l - 2))) * summation);
+end
+
+function [result] = process_subband_matrix(CoeffMatrix, sigma)
+    %For each sub matrix find the maximum value and use it to calculate
+    %variable m (lowercase), this is based upon the following paper.
+    % WAVELET IMAGE DE-NOISING METHOD BASED ON NOISE STANDARD DEVIATION ESTIMATION 
+    Mij = max(CoeffMatrix(:));
+    K = 1;
+    m = K*(Mij - sigma);
+    
+    %Loop on each value within the CoeffMatrix and apply the yalpha
+    %function to each value and then multiply the results by the output
+    CoeffMatrixSize = size(CoeffMatrix);
+    for y=1:CoeffMatrixSize(1)
+        for x=1:CoeffMatrixSize(2)
+            CoeffValue = CoeffMatrix(y, x);
+            new_CoeffValue = yalpha(CoeffValue, sigma, m);
+            CoeffMatrix(y, x) = CoeffValue * new_CoeffValue;
+        end
+    end
+    
+    result = CoeffMatrix;
+end
+
+function [result] = yalpha(x, sigma, m)
+    a = 3;
+    p = 1;
+    q = 1;
+    c = sigma;
+    
+    if(abs(x) < (a*c))
+        result = 1;
+    elseif((a*c) <= abs(x) && abs(x) < (2*a*c))
+        result = ((abs(x) - (a*c) / (a*c)) * ((m / (a*c))^p)) + (((2*a*c) - abs(x)) / (a*c));
+    elseif((2*a*c) <= abs(x) && abs(x) < m)
+        result = ((m / abs(x))^p);
+    elseif(m <= abs(x))
+        result = ((m / abs(x))^q);
+    else
+        disp('Error in yalpha');
+    end
 end
 
 function [finalimg] = add_img(inputimg, M, finalimg)
