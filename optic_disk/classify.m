b358743b01feb75512eee4aa9c0c81102b069b18
patnatha <patnatha@umich.edu>
2013-09-25 17:57:22 -0400

    ok

--- optic_disk/classify.m
+++ optic_disk/classify.m
@@ -1,5 +1,12 @@
 function classify(image)
     load('svm_model.mat', 'SVMstruct');
-
     
+    window_size = size(image, 1) / 3;
+    
+    for y=1:(size(image, 1) - window_size)
+        subimage = image(y:y+window_size, 1:window_size);
+        hogin = HOG(subimage);
+        group = svmclassify(SVMstruct, transpose(hogin));
+        disp([num2str(y), ': ', num2str(group)]);
+    end
 end
