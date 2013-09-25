7b886adbce1f95659ba8b11644fe8d1bb35ca30d
Nathan Patel <nathanpatel@Nathans-MacBook-Air.local>
2013-09-25 17:52:19 -0400

    yeah yeah

--- optic_disk/classify.m
+++ optic_disk/classify.m
@@ -1,5 +1,14 @@
 function classify(image)
     load('svm_model.mat', 'SVMstruct');
 
+    hogin = HOG(image);
     
+    window_size = size(image, 1) / 3;
+    
+    for y=1:(size(image, 1) - window_size)
+        subimage = image(y:y+window_size, 1:window_size);
+        hogin = HOG(subimage);
+        group = svmclassify(SVMstruct, transpose(hogin));
+        disp([num2str(y), ': ', num2str(group)]);
+    end
 end
