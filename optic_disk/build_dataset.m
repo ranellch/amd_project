c1a75f0a21dfb6e5f3d1f121ce5aeb684aa50880
patnatha <patnatha@umich.edu>
2013-09-30 22:32:44 -0400

    rady to run auto build

--- optic_disk/build_dataset.m
+++ optic_disk/build_dataset.m
@@ -35,82 +35,102 @@ for count=1:images.getLength
     the_path = char(image.getAttribute('path'));
     image = imread(the_path);
     
+    %Calculate the size of the 
+    number_of_pixels_per_box = 16;
+    subimage_size = floor(size(image, 1) / number_of_pixels_per_box);
+    
     if isKey(mapObjExclude, the_path)
         disp(['Skipping: ', the_path]);
         continue;
     else
-        disp(['Loading: ', the_path]);
+        disp(['Loading: ', the_path, ' - Box Count: ', num2str(subimage_size)]);
     end
 
     if isKey(mapObj, the_path) == 0
         mapObj(the_path) = 0.0;
     end
     
-    maxy = size(image, 1);
-    maxx = size(image, 2);
-    minimum_size = maxy / 3;
+    last_part_in_path = strfind(the_path, '/');
+    last_index = 1;
+    if ~isempty(last_part_in_path)
+        last_index = last_part_in_path(length(last_part_in_path));
+    end
+    
+    %Get the snaked file
+    snaked_file_name = ['snaked/', the_path(last_index:length(the_path))];
+    snaked_image = im2bw(imread(snaked_file_name));
+        
+    fileID = fopen('train.dataset','at');
     
     subimages_count = 1;
-    found_optic_disk = 0;
-    while subimages_count <= 10
-        if mapObj(the_path) < subimages_count
-            [xs, xe, ys, ye] = random_box(maxx, maxy, minimum_size);
-            subimage = image(ys:ye, xs:xe);
+    for x=1:subimage_size
+        for y=1:subimage_size
+            if mapObj(the_path) < subimages_count
+                xs = ((x - 1) * number_of_pixels_per_box) + 1;
+                xe = xs + number_of_pixels_per_box - 1;
+                
+                ys = ((y - 1) * number_of_pixels_per_box) + 1;
+                ye = ys + number_of_pixels_per_box - 1;
+                
+                if(ye <= size(image, 1) && xe <= size(image, 2))              
+                    %Get the original image
+                    subimage = image(ys:ye, xs:xe);
 
-            figure(2);
-            imshow(subimage);
-            yesnobutton = questdlg('Does this image contain an optic disc?', [the_path, ' - ', num2str(subimages_count)],'Yes','No', 'Cancel', 'Cancel');
-            switch yesnobutton
-                case 'Yes'
-                    found_optic_disk = found_optic_disk + 1;
-                    subimages_count = subimages_count - 1;
-                case 'No'
-                    fileID = fopen('train.dataset','at');
-                    fprintf(fileID, '"%s" %d, 0, %s\n', the_path, subimages_count, hog_to_string(subimage), ',', lbp_to_string(subimage));
-                    fclose(fileID);
-                case 'Cancel'
-                    return;
+                    %Get the snake image
+                    subimage_snake = snaked_image(ys:ye, xs:xe);
+                    
+                    %Get the percentage of the disk included in this image
+                    percentage_disk = sum(subimage_snake(:)) / (number_of_pixels_per_box * number_of_pixels_per_box);
+                                        
+                    if(percentage_disk > 0.7)
+                        fprintf(fileID, '"%s" %d, 1, %s\n', the_path, subimages_count, lbp_to_string(subimage));
+                    else
+                        fprintf(fileID, '"%s" %d, 0, %s\n', the_path, subimages_count, lbp_to_string(subimage));
+                    end
+                    
+                    mapObj(the_path) = subimages_count;
+                end
             end
-            close 2;
-            mapObj(the_path) = subimages_count;
+            subimages_count = subimages_count + 1;
         end
-        subimages_count = subimages_count + 1;
     end
     
-    if mapObj(the_path) < 11
-        figure(1);
-        imshow(image);
-        [centerx, centery] = ginput(1);
-        close 1;
-        
-        xs = centerx(1) - (minimum_size / 2);
-        if(xs <= 0)
-            xs = 1;
-        end
-        xe = xs + minimum_size;
-        
-        ys = centery(1) - (minimum_size / 2);
-        if(ys <= 0)
-            ys = 1;
-        end
-        ye = ys + minimum_size;
-        
-        optic_disk_image = image(ys:ye, xs:xe);
-        figure(3);
-        imshow(optic_disk_image);
-        yesnobutton = questdlg('Does this image contain an optic disc?','Optic Disc?','Yes','No', 'Cancel', 'Cancel');
-        switch yesnobutton
-            case 'Yes'
-                found_optic_disk = found_optic_disk + 1;
-                fileID = fopen('train.dataset','at');
-                fprintf(fileID, '"%s" 11, 1, %s\n', the_path, hog_to_string(optic_disk_image), ',', lbp_to_string(optic_disk_image));
-                fclose(fileID);
-            case 'No'
-                
-            case 'Cancel'
-                return;
-        end
-        close 3;
-    end
+    fileID = fopen('train.dataset','at');
+    
+%     if mapObj(the_path) < 11
+%         figure(1);
+%         imshow(image);
+%         [centerx, centery] = ginput(1);
+%         close 1;
+%         
+%         xs = centerx(1) - (minimum_size / 2);
+%         if(xs <= 0)
+%             xs = 1;
+%         end
+%         xe = xs + minimum_size;
+%         
+%         ys = centery(1) - (minimum_size / 2);
+%         if(ys <= 0)
+%             ys = 1;
+%         end
+%         ye = ys + minimum_size;
+%         
+%         optic_disk_image = image(ys:ye, xs:xe);
+%         figure(3);
+%         imshow(optic_disk_image);
+%         yesnobutton = questdlg('Does this image contain an optic disc?','Optic Disc?','Yes','No', 'Cancel', 'Cancel');
+%         switch yesnobutton
+%             case 'Yes'
+%                 found_optic_disk = found_optic_disk + 1;
+%                 fileID = fopen('train.dataset','at');
+%                 fprintf(fileID, '"%s" 11, 1, %s\n', the_path, hog_to_string(optic_disk_image), ',', lbp_to_string(optic_disk_image));
+%                 fclose(fileID);
+%             case 'No'
+%                 
+%             case 'Cancel'
+%                 return;
+%         end
+%         close 3;
+%     end
 end
 
