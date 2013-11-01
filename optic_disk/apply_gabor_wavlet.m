134041cc7ec8ff4a74955f1f80d3d4511bfa4ce3
Nathan Patel <nathanpatel@Nathans-MacBook-Air.local>
2013-11-01 11:42:56 -0400

    working on gabor wavelets stuff

--- optic_disk/apply_gabor_wavlet.m
+++ optic_disk/apply_gabor_wavlet.m
@@ -1,25 +1,28 @@
 function [WaveletPieces] = apply_gabor_wavlet(img)
     addpath('GaborWavelet');
     
+    %Convert image to double image
     img = double(img);
 
-    scales = 0.5;
-    orientations = [0, pi/3.0, (2.0*pi) / 3.0, pi, (4.0 * pi) / 3.0, (5.0 * pi) / 3.0];
-    psi = 90.0;
-    lambda = 8.0;
+    lambda_wavelength = 8;
+    sigma_stddev = 0.56 * lambda_wavelength;
+    theta_orient = [0, pi/3.0, (2.0*pi) / 3.0, pi, (4.0 * pi) / 3.0, (5.0 * pi) / 3.0];
+    phi_phase_offset = [0, 90];
+    gamma_aspect_ratio = 0.5;
+    bandwidth = 0;
     
-    WaveletPieces = zeros(size(img, 1), size(img, 2), size(orientations, 2));
+    WaveletPieces = zeros(size(img, 1), size(img, 2), size(theta_orient, 2), size(phi_phase_offset, 2));
     current_piece = 1;
     
-    %for v = 0 : size(scales);
-        for u = 1 : size(orientations, 2);
-            GW = GaborWavelet (0.56 * lambda, orientations(u), lambda, psi, scales); % Create the Gabor wavelets
-            orien_scale = conv2(img, GW); %Convolve the gabor wavelet with the img
+    for v=1:size(phi_phase_offset, 2);
+        for u=1:size(theta_orient, 2);
+            GW = gaborfilter(img, lambda_wavelength, sigma_stddev, theta_orient(u), phi_phase_offset(v), gamma_aspect_ratio, bandwidth);
+            figure(current_piece), imshow(GW);
             
             %Keep this convolution in a matrix
             for y=1:size(img, 1)
                 for x=1:size(img,2)
-                    WaveletPieces(y, x, current_piece) = orien_scale(y, x);
+                    WaveletPieces(y, x, current_piece) = GW(y, x);
                 end
             end
             
@@ -28,5 +31,5 @@ function [WaveletPieces] = apply_gabor_wavlet(img)
             %Update the current piece
             current_piece = current_piece + 1;
         end
-    %end
+    end
 end
