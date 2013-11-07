c0448889e5fddb0f792464d2163a9e441e8c9ef4
patnatha <patnatha@umich.edu>
2013-11-07 09:56:06 -0500

    trying to encapsulte the line operator

--- optic_disk/build_dataset_vessels.m
+++ optic_disk/build_dataset_vessels.m
@@ -1,11 +1,35 @@
-function build_dataset_vessels()
+function build_dataset_vessels(gabor_bool, lineop_bool)
+    %Test the values of the input variables
+    if((gabor_bool == 0 || gabor_bool == 1) && ...
+       (lineop_bool == 0 || lineop_bool == 1))
+        if((gabor_bool == 0 && lineop_bool == 0) || ...
+           (gabor_bool == 1 && lineop_bool == 1))
+            error('You must select one and only one feature to build training set.');
+        end
+    else
+        error('Input variables must be the value 0 or 1.');
+    end
+
+    %Filename constants
     filename_input = 'vessel_draw.dataset';
     filename_gabor = 'gabor.classifier';
     filename_lineop = 'lineop.classifier';
 
+    %Remove gabor file is already exists
+    if(gabor_bool == 1 && exist(filename_gabor, 'file') == 2)
+        delete(filename_gabor);
+    end
+
+    %Remove lineop file if already exists
+    if(lineop_bool == 1 && exist(filename_lineop, 'file') == 2)
+        delete(filename_lineop);
+    end
+
     addpath(genpath('../Test Set'));
     addpath('..');
-    
+
+    t = cputime;   
+ 
     %Get the images already run in the 
     mapObj = containers.Map('KeyType', 'char', 'ValueType', 'int32');
     try
@@ -15,8 +39,8 @@ function build_dataset_vessels()
         fclose(fid);
        
         %Open the output files for writing 
-        fout = fopen(filename_gabor, 'at');
-        flineop = fopen(filename_lineop, 'at');       
+        if(gabor_bool == 1) fout = fopen(filename_gabor, 'w'); end
+        if(lineop_bool == 1) flineop = fopen(filename_lineop, 'w'); end    
 
         %Iterate over all images to use for training 
         for k=1:size(paths, 1)
@@ -51,15 +75,19 @@ function build_dataset_vessels()
             %For each pixel build a classifier
             for y=1:size(orig_wavelets,1)
                 for x=1:size(orig_wavelets,2)
-                    %Get the gabor wavelet feature vector
-                    feature_vector_gabor=zeros(size(orig_wavelets, 3), 1);
-                    for wave=1:size(orig_wavelets, 3)
-                        feature_vector_gabor(wave, 1) = orig_wavelets(y,x,wave);
+                    if(gabor_bool == 1)
+                        %Get the gabor wavelet feature vector
+                        feature_vector_gabor=zeros(size(orig_wavelets, 3), 1);
+                        for wave=1:size(orig_wavelets, 3)
+                            feature_vector_gabor(wave, 1) = orig_wavelets(y,x,wave);
+                        end
                     end
 
-                    %Get the line operator feature vector
-                    feature_vector_lineop = line_operator(original_img, y, x, 15, 8)';
- 
+                    if(lineop_bool == 1)
+                        %Get the line operator feature vector
+                        feature_vector_lineop = line_operator(original_img, y, x, 15, 8)';
+                    end
+
                     %Get the grouping for this particular pixel
                     grouping = 0;
                     if(y <= size(vesselized_img_binary, 1) && x <= size(vesselized_img_binary, 2))
@@ -72,13 +100,18 @@ function build_dataset_vessels()
                     if(x > border_ignore && x < (size(orig_wavelets,2) - border_ignore) && ...
                        y > border_ignore && y < (size(orig_wavelets,1) - border_ignore) && ...
                        (grouping == 1 || random_sample == 4))
+        
                         %Write to the output file the gabor wavelet string
-                        feature_string_gabor=feature_to_string(feature_vector_gabor);
-                        fprintf(fout, '%d,%s\n', grouping, feature_string_gabor);
+                        if(gabor_bool == 1)
+                            feature_string_gabor=feature_to_string(feature_vector_gabor);
+                            fprintf(fout, '%d,%s\n', grouping, feature_string_gabor);
+                        end
 
                         %Write to the output file the line operator string
-                        feature_string_lineop=feature_to_string(feature_vector_lineop);
-                        fprintf(flineop, '%d %s\n', grouping, feature_string_lineop);
+                        if(lineop_bool == 1)
+                            feature_string_lineop=feature_to_string(feature_vector_lineop);
+                            fprintf(flineop, '%d,%s\n', grouping, feature_string_lineop);
+                        end 
 
                         random_sample = 1;
                         if(grouping == 1)
@@ -93,11 +126,13 @@ function build_dataset_vessels()
             end
         end
         
-        fclose(fout);
-        fclose(flineop);
+        if(gabor_bool == 1) fclose(fout); end
+        if(lineop_bool == 1) fclose(flineop); end
     catch err
         disp(err.message);
     end
     
     disp(['Ones: ', num2str(grouping_one), ' - Zeros: ', num2str(grouping_zero)]);
+    e = cputime - t;
+    disp(['Time to build dataset (min): ', num2str(3 / 60.0)]);
 end
--- optic_disk/line_operator.m
+++ optic_disk/line_operator.m
@@ -1,3 +1,138 @@
+classdef line_operator
+properties(GetAccess=private)
+    len
+    norien
+    line_matricies
+    degrees
+    ready_to_go
+end
+
+methods
+    function obj=line_operator(l, nor)
+        %Error check the input values
+        if(mod(l, 2) == 0)
+            error('The length(l) must be odd for this to work');
+        end
+        if(nor <= 0)
+            error('The number of orientations(norien) must be greater than 0');
+        end
+  
+        %Get the line definition variables
+        len = l;
+        norien = nor;
+
+        %Init the line_matricies variable
+        line_matricies = zeros(len,len,norien);
+        degress = zeros(len, 1);
+
+        for theta=1:norien
+            %Get the current degree orientation of the line
+            deg = (theta / norien) * 180.0;
+
+            %Get the line matrix and copy into list of line matricies
+            sub_line_matrix = create_line(len, deg);
+            for y=1:len
+                for x=1:len
+                    line_matricies(y,x,theta) = sub_line_matrix(y,x);
+                end
+            end
+
+            degrees(theta, 1) = deg;
+        end
+
+        ready_to_go = 1;
+    end
+
+    function [fv] = get_fv(img, yin, xin)
+        if(ready_to_go ~= 1)
+            error('Class has not been initialized yet');
+        end
+
+        %Variables for output
+        fv = zeros(1, 3);
+        ed = floor(len / 2.0);
+
+        %Get the average gray scale intensity within the square window over the xurrent pixel under investigation
+        [square_sum, square_count] = iterate_mask(img, yin, xin, each_direction, ones(l));
+        square_avg = square_sum / double(square_count);
+
+        %Find line orientation with maximum line strength
+        max_line_strength = 0.0;
+        max_line_str_deg = 0.0;
+
+        for theta=1:norien
+            %Calculate the current line strength
+            [line_sum, line_count] = iterate_mask(img, yin, xin, ed, line_matrcies(:,:,theta));
+            line_avg = line_sum / double(line_count);
+            current_line_strength = line_avg - square_avg;
+
+            %Keep track of greatest line strength
+            if(current_line_strength > max_line_strength)
+                max_line_strength = current_line_strength;
+                max_line_str_deg = degrees(theta, 1);
+            end
+        end
+
+        %Get the line strength of the pixel perpendicular to the maximum line strength
+        nindeg = max_line_str_deg + 90.0;
+        ninlen = 3;
+        nindeg_matrix = create_line(ninlen, nindeg);
+        [nine_sum, nine_count] = iterate_mask(img, yin, xin, floor(ninlen / 2.0), nindeg_matrix);
+        nine_avg = nine_sum / double(nine_count);
+        nine_line_strength = nine_avg - square_avg;
+
+        fv(1,1) = max_line_strength;
+        fv(1,2) = nine_line_strength;
+        fv(1,3) = img(yin,xin);
+    end
+
+    function [line_matrix] = create_line(length, angle)
+       %Create a line with at least l points that are the value one
+        line_matrix_temp = strel('line', length, angle).getnhood();
+        next_size_up=1;
+        while(size(line_matrix_temp, 1) < length && size(line_matrix_temp, 2) < length)
+            line_matrix_temp = strel('line', length + next_size_up, angle).getnhood();
+            next_size_up = next_size_up+1;
+        end
+
+        %Pad array becuase we will later extract a square that is lengthxlength
+        ed = floor(length / 2.0);
+        line_matrix = padarray(line_matrix_temp, [ed,ed], 'both');
+
+        %Calculate the index of the center of the matrix
+        middle_y = ceil(size(line_matrix, 1) / 2.0);
+        middle_x = ceil(size(line_matrix, 2) / 2.0);
+
+        %Get the subset martix
+        line_matrix = line_matrix(middle_y-ed:middle_y+ed, middle_x-ed:middle_x+ed);
+    end
+
+    function [sumval, count] = iterate_mask(img, yin, xin, each_direction, matrix_in)
+        %Initialize variables for summation and counting
+        sumval = 0.0;
+        count = 0;
+
+        start_y = 0;
+        for y = yin - each_direction:yin + each_direction
+            start_y = start_y + 1;
+            start_x = 0;
+            for x = xin - each_direction:xin + each_direction
+                start_x = start_x + 1;
+                if(y >= 1 && y<=size(img, 1) && x >= 1 && x <= size(img, 2))
+                    if(matrix_in(start_y, start_x) == 1)
+                        count = count + 1;
+                        sumval = sumval + double(img(y, x));
+                    end
+                end
+            end
+        end
+    end
+   
+ 
+end
+
+end
+
 function [feature_vector] = line_operator(img, yin, xin, l, norien)
     %Error check the input values
     if(mod(l, 2) == 0)
@@ -61,7 +196,7 @@ function [feature_vector] = line_operator(img, yin, xin, l, norien)
 
     feature_vector(1,1) = max_line_strength;
     feature_vector(1,2) = nine_line_strength;
-    feature_vector(1,3) = img(y,x);
+    feature_vector(1,3) = img(yin,xin);
 end
 
 function [line_matrix] = create_line(length, angle)
