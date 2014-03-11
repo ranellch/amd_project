689e4955165d6cc5801ea6e4ab24e2af1121f794
Christopher Ranella <ranellch@caen-vnc12.engin.umich.edu>
2014-03-10 20:15:23 -0400

    registration almost done

--- vessel_detection/align_images_coor.m
+++ vessel_detection/align_images_coor.m
@@ -41,11 +41,11 @@ function [pointsA, pointsB] = align_images_coor(img1, img2, quad_count, skip_qua
     %Get the most common points in each quad
     temp = most_common(cc.corresps, quad_count, skip_quad, minx, miny);
     
-    %Display the original set of matched points
-    figure(1);
-    correspDisplay(temp, image1);
-    %figure(2);
-    %correspDisplay(temp, image2);
+%     %Display the original set of matched points
+%     figure(1);
+%     correspDisplay(temp, image1);
+%     %figure(2);
+%     %correspDisplay(temp, image2);
     
     %Form arry in the correct manner
     pointsA = temp(1:2,:)';
