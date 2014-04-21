fe25e811af5361d61e983c02f0be4205c032bd32
Chris Ranella <ranellch@umich.edu>
2014-04-21 19:27:25 -0400

    modified testing protocol, added new files

--- optic_disk/analyze_vessels_leave_one.m
+++ optic_disk/analyze_vessels_leave_one.m
@@ -1,65 +1,41 @@
-function analyze_vessels(rebuild_classifier)
+function analyze_vessels_leave_one()
     addpath('vessel_draw');
     addpath('..');
     
     results_file = 'analyze_results.txt';
     
-    if(rebuild_classifier == 1)
-        %Build training set
-        build_dataset_vessels(1,0);
-        build_dataset_vessels(0,1);
-
-        %Train the classifier
-        train_vessels();
-    end
-    
-    %Open the file to test all of this against
-    test_file = 'vessel_draw_test.dataset';
-    fid = fopen(test_file);
-        
-    %Get the first line of the file
-    tline = fgetl(fid);
-    if(isnumeric(tline) == 1 && tline == -1)
-        disp(['Check the contents of ', test_file, ' it appears to be empty!']);
-        return;
-    else
-        disp(['Reading: ', test_file]);
-    end
-    disp('-----------------------------');
-    
-    %Close the file
-    fclose(fid);
-    
-    %Open the file to determine which images to use for testing 
-    fid = fopen(test_file, 'r');
+    %get how many vessel images we have
+    filename_input = 'vessel_draw.dataset';
+    fid = fopen(filename_input, 'r');
     paths = textscan(fid,'%q %d %q %*[^\n]');
     fclose(fid);
     
-    %Run through the images and make sure that they exist
-    for k=1:size(paths, 2)
+    %Loop through and test classifiers using the "leave one out" method
+    for k = 1:size(paths,2)
+        
+        %Specify test image info
         pid = char(paths{1}{k});
         time = num2str((paths{2}(k)));
         vessel_image = char(paths{3}{k});
-            
-        %See if original image exists
-        img_path_exists = get_path(pid, time);
         
-        %Get the image traced by hand
-        super_img = imread(vessel_image);
-    end
-    
-    output_results = zeros(size(paths, 1), 8);
+        %Specify training images
+         rowindex=ones(size(paths,2),1);
+         rowindex(k)=0;
+         training_paths = paths(rowindex~=0,:);
         
-    
-    %Iterate over all images to use for training 
-    for k=1:size(paths, 2)
-        pid = char(paths{1}{k});
-        time = num2str((paths{2}(k)));
-        vessel_image = char(paths{3}{k});
-       
+        %Build training set
+        build_dataset_vessels_leave_one(1,0, training_paths);
+        build_dataset_vessels_leave_one(0,1, training_paths);
+
+        %Train the classifier
+        train_vessels();
+
+        %Test all of this against left out image
+        output_results = zeros(size(paths, 1), 8);
+
         %Get the image run by the algorithm
         calced_img = find_vessels(pid, time, 0);
-        
+
         %Get the image traced by hand
         super_img = imread(vessel_image);
         total_positive_count = 0;
@@ -73,7 +49,7 @@ function analyze_vessels(rebuild_classifier)
                 end
             end
         end
-        
+
         %Check the sizing of the images compared to each other
         if(size(calced_img, 1) ~= size(super_img, 1) || size(calced_img, 2) ~= size(super_img, 2))
             disp(['Images Not Same Size: ', pid, ' - ', time]);
@@ -101,12 +77,12 @@ function analyze_vessels(rebuild_classifier)
                 total_count = total_count + 1;
             end
         end
-        
+
         if(total_count ~= (total_negative_count + total_positive_count))
             disp(['total_count (', num2str(total_count),') and total_negative + total_positive_count (', num2str(total_negative_count + total_positive_count),') Do not match']);
             continue;
         end
-        
+
         output_results(k,1) = true_positive;
         output_results(k,2) = true_negative;
         output_results(k,3) = false_positive;
@@ -117,6 +93,7 @@ function analyze_vessels(rebuild_classifier)
         output_results(k,8) = true_positive/(true_positive+false_positive); %precision
         disp('--------------------------------------');
     end
+
 
     fout = fopen(results_file, 'w');
     
