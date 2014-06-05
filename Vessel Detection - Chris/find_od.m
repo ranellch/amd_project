b164e9f74fce6d1297f44481801c6de36b947e0f
patnatha <patnatha@umich.edu>
2014-06-05 13:01:52 -0400

    added some useful output informaiton to the script

--- Vessel Detection - Chris/find_od.m
+++ Vessel Detection - Chris/find_od.m
@@ -7,11 +7,14 @@ std_img_size = 768;
 addpath('..');
 addpath(genpath('../Test Set'));
 addpath('../intensity normalization');
-addpath(genpath('../sfta'));
 addpath('../snake');
-run('../vlfeat/toolbox/vl_setup');
 addpath(genpath('../liblinear-1.94'))
         
+%Load the prediction structs
+model = load('od_classify_svmstruct.mat');
+scaling_factors = model.scaling_factors;
+classifier = model.od_classify_svmstruct;
+
 %Get the path name from the image and time and then read in the image.
 filename = get_pathv2((pid), (eye), num2str(time), 'original');
 img = imread(filename);
@@ -26,7 +29,7 @@ if(size(img,3) ~= 1)
     img=rgb2gary(img);
 end
 
-%Apply a gaussian filter to the image
+%Apply a gaussian filter to the image and the smooth out the illumination
 img = gaussian_filter(img);
 [img, ~] = smooth_illum3(img, 0.7);
 
@@ -38,20 +41,15 @@ img = match_sizing(img, std_img_size, std_img_size);
 %Print to the console the output
 disp(['ID: ', pid, ' - Time: ', num2str(time)]);
 
-%Load the prediction structs
-model = load('od_classify_svmstruct.mat');
-scaling_factors = model.scaling_factors;
-classifier = model.od_classify_svmstruct;
-
-x=-1;
-y=-1;
-
+%Initiate the results image
 od_image = zeros(size(img, 1), size(img, 2));
 
 %Get feature vectors for each pixel in image
+disp('[FV] Building the pixelwise feature vectors');
 feature_image_g = get_fv_gabor(img);
 feature_image_r = rangefilt(img);
 
+%Build the finalized feature vector
 feature_image = zeros(size(od_image,1), size(od_image,2), size(feature_image_g,3) + size(feature_image_r,3));
 
 for y=1:size(feature_image, 1)
@@ -68,10 +66,10 @@ for y=1:size(feature_image, 1)
     end
 end
 
-%convert this feature image into a flat array of feature vectos
+%convert this feature image into a flat array of feature vectors
 instance_matrix = matstack2array(feature_image);
 
-%Scale vectors
+%Scale the vectors for input into the classifier
 for i = 1:size(instance_matrix,2)
     fmin = scaling_factors(1,i);
     fmax = scaling_factors(2,i);
@@ -79,30 +77,23 @@ for i = 1:size(instance_matrix,2)
 end
 
 %Run the classification algorithm
+disp('[SVM] Running the classification algorithm');
 class_estimates = libpredict(zeros(length(instance_matrix),1), sparse(instance_matrix), classifier);
-
-%Classify the feature vectors for each pixel
-%for y=1:size(feature_image, 1)
-%    temp_rearrange = squeeze(feature_image(y,:,:));
-%    class_estimates = svmclassify(od_classify_svmstruct, temp_rearrange);
-%    for x=1:size(feature_image, 2)
-%        od_image(y,x) = class_estimates(x);
-%    end
-%end
-
 od_image(:) = class_estimates;
 
-%Close the image so that all the little pices close together become connected
+%Close the image so that all the little pieces close together become connected
 od_image = imclose(od_image, strel('disk',5));
 od_image = imfill(od_image,'holes');
 
 %Remove the smaller disconnected regions as they are not likely to be an optic disc
 od_image = imopen(od_image, strel('disk', 5));
     
-%Refine the possibilites of the optic disc
+%Refine the possibilites of the optic disc using a voting algorithm
+disp('[VOTING] Using vessel endpoint voting to find the true optic disc');
 pre_snaked_img = refine_od(od_image, img_vessel);
 
 %Use snaking algorithm to get smooth outline of the optic disc
+disp('[SNAKES] Using Snaking algorithm to refine the edges of the optic disc');
 Options=struct;
 Options.Verbose=false;
 Options.Iterations=200;
@@ -113,14 +104,15 @@ Points = get_box_coordinates(pre_snaked_img);
 %Resize the image to its original size
 snaked_optic_disc = match_sizing(snaked_optic_disc, origx, origy);
 
-imshowpair(snaked_optic_disc, img);
-
 %return the final image to the function caller
 final_od_image = snaked_optic_disc;
 
+%Report the time it took to classify to the user
 e = cputime - t;
-disp(['Optic Disc Classification Time (sec): ', num2str(e)]);
-    
+disp(['[TIME] Optic Disc Classification Time (sec): ', num2str(e)]);
+
+imshowpair(snaked_optic_disc, img);
+
 end
 
 function other()
