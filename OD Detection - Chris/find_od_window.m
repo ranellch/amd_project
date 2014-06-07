6b90ee44929fb5c2bd32d532f1fbcc1091b9f47b
ranellch <ranellch@umich.edu>
2014-06-07 03:00:09 -0400

    find_od update

--- OD Detection - Chris/find_od_window.m
+++ OD Detection - Chris/find_od_window.m
@@ -8,19 +8,27 @@ addpath('../Vessel Detection - Chris');
 %Get vessels and angles of greatest lineop strength
 [vessels, angles] = find_vessels(pid, eye, time);
 figure, imshow(vessels)
-
-[origy, origx] = size(angles);
 vskel = bwmorph(vessels,'skel',Inf);
 angles(~vskel) = 0;
+
+[origy, origx] = size(angles);
 angles = mod(angles,180);
-angles = padarray(angles, [origy origx], 'symmetric', 'both');
+ maxpad = 100;
+ angles = padarray(angles, [maxpad maxpad], 'symmetric', 'both');
+% 
+ %adjust "mirroring" so angles translate over
+angles(1:maxpad,maxpad:2*maxpad-1) = 180 - angles(1:maxpad,maxpad:2*maxpad-1);
+angles(2*maxpad:2*maxpad+maxpad-1,maxpad:2*maxpad-1) = 180 - angles(2*maxpad:2*maxpad+maxpad-1,maxpad:2*maxpad-1);
+angles(maxpad:2*maxpad-1,1:maxpad) = 180 - angles(maxpad:2*maxpad-1,1:maxpad);
+angles(maxpad:2*maxpad-1,2*maxpad:2*maxpad+maxpad-1) = 180 - angles(maxpad:2*maxpad-1,2*maxpad:2*maxpad+maxpad-1); 
+angles = mod(angles,180);
 
 %Interpolate
 [y, x, angs] = find(angles);
 [xq, yq] = meshgrid(1:size(angles,2), 1:size(angles,1));
 angle_map = griddata(x, y, angs, xq, yq,'cubic');
-angle_map = angle_map(origy+1:origy+origy,origx+1:origx+origx);
-% figure, imshow(mat2gray(angle_map))
+angle_map = angle_map(maxpad+1:maxpad+origy,maxpad+1:maxpad+origx);
+ figure, imshow(mat2gray(angle_map))
 
 %Run correlation on this mofo
 od_filter = load('od_masks', 'mask200', 'mask300', 'mask400');
@@ -30,10 +38,33 @@ e = cputime;
 scales = [200 300 400];
 diff_img = zeros(origy, origx,length(scales));
 for k = 1:length(scales)
-    angle_map = padarray(angle_map, [scales(k)/2 scales(k)/2], 'symmetric', 'both');
+%     angle_map = padded_angle_map(maxpad+1-scales(k)/2:origy+maxpad+scales(k)/2,maxpad+1-scales(k)/2:origx+maxpad+scales(k)/2);
+    full_mask = od_filter.(['mask',num2str(scales(k))]);
     for y = 1:16:768
+        tb = y - scales(k)/2;
+        bb = y + scales(k)/2-1;
+        ymask = full_mask;
+        if y < scales(k)/2
+            ymask = full_mask(scales(k)/2+1-(y-1):end,:);
+            tb = 1;
+        end
+        if y > origy - scales(k)/2
+            ymask = full_mask(1:scales(k)-(y+scales(k)/2-origy)+1,:);
+            bb = origy;
+        end
         for x = 1:16:768
-            diff_img(y,x,k) = corr2(angle_map(y:y+scales(k)-1,x:x+scales(k)-1),od_filter.(['mask',num2str(scales(k))]));
+            lb = x - scales(k)/2;
+            rb = x + scales(k)/2-1;
+            mask = ymask;
+            if x < scales(k)/2
+                mask = ymask(:,scales(k)/2+1-(x-1):end);
+                lb = 1;
+            end
+            if x > origx - scales(k)/2
+                mask = ymask(:,1:scales(k)-(x+scales(k)/2-origx)+1);
+                rb = origx;
+            end        
+            diff_img(y,x,k) = corr2(angle_map(tb:bb,lb:rb),mask);
         end
     end
 end
