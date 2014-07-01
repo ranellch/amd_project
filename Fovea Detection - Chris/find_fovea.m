8b003ee24fa66bdd0263e1a230574839e77260cd
unknown <ranellch@umich.edu>
2014-07-01 16:22:11 -0400

    parabola bugs all worked out

--- Fovea Detection - Chris/find_fovea.m
+++ Fovea Detection - Chris/find_fovea.m
@@ -36,45 +36,40 @@ Par = CircleFitByTaubin([x,y]);
 xc = Par(1);
 yc = Par(2);
 
+%Put image in normal coordinates centered at optic disk
+[xcorr,ycorr] = meshgrid((1:size(vessels,2)) - xc, yc - (1:size(vessels,1)));
+
 %find all points of thick vessels (>6 pixels)
-figure, imshow(v_thicknesses>6)
-[y,x] = find(v_thicknesses>6);
+% figure, imshow(v_thicknesses>6)
+points = find(v_thicknesses>7);
+x = xcorr(points);
+y = ycorr(points);
 
 %find parameters a and b to best fit parabola
-a0 = 0.002;
+a0 = 0.003;
 B0 = 0;
 options = optimoptions('lsqnonlin');
-options.Algorithm = 'levenberg-marquardt';
-options.TolFun = 1e-12;
+% options.Algorithm = 'levenberg-marquardt';
+options.TolFun = 1e-9;
 params = lsqnonlin(@parabola_criterion,[a0,B0],[],[],options);
 a = params(1)
 B = params(2)
 
-%put entire image in new coordinate system
 xprime = zeros(size(vessels));
 yprime = zeros(size(vessels));
-for y = 1:size(vessels,1)
-    for x = 1:size(vessels,2)
+%put entire image in new coordinate system
+for i = 1:size(vessels,1)
+    for j = 1:size(vessels,2)
         if B == 0
-            xprime(y,x) = x;
-            yprime(y,x) = y;
+            xprime(i,j) = xcorr(i,j);
+            yprime(i,j) = ycorr(i,j);
         else
-            xprime(y,x) = (x*cosd(B)+y*sind(B))/(cosd(B)^2+sind(B)^2);
-            yprime(y,x) = (xprime(y,x)*cosd(B)-x)/sind(B);
+            xprime(i,j) = (xcorr(i,j)*cosd(B)+ycorr(i,j)*sind(B))/(sind(B)^2+cosd(B)^2);
+            yprime(i,j) = (xprime(i,j)*cosd(B)-xcorr(i,j))/sind(B);
         end
     end
 end
 
-%center at optic disk
-if B == 0
-    xcprime = xc;
-    ycprime = yc;
-else
-    xcprime = (xc*cosd(B)+yc*sind(B))/(cosd(B)^2+sind(B)^2);
-    ycprime = (xcprime*cosd(B)-xc)/sind(B);
-end
-xprime = xprime - xcprime;
-yprime = yprime - ycprime;
 
 %plot parabola
 if debug == 2
@@ -82,25 +77,32 @@ if debug == 2
     f_y = zeros(length(y_domain),2);
     f_y(:,1) = round(a*y_domain.^2);
     f_y(:,2) = round(-1*a*y_domain.^2);
-    figure(8)
-    imshow(vessels)
-    hold on
-    for i = 1:length(f_y)
-        [y,x] = find(round(yprime)==round(y_domain(i))&round(xprime)==round(f_y(i,1)));
-        if ~isempty(y)
-            plot(x,y,'r.')
-        end
-        [y,x] = find(round(yprime)==round(y_domain(i))&round(xprime)==round(f_y(i,2)));
-        if ~isempty(y)
-            plot(x,y,'r.')
-        end
-        [y,x] = find(round(yprime)==0);
-        if ~isempty(y)
-            plot(x,y,'b-')
+    figure(8);
+    imshow(vessels);
+     hold on
+    for y = 1:size(yprime,1)
+        for x = 1:size(xprime,2)
+            for i = 1:length(y_domain) 
+                 if (round(xprime(y,x)) == f_y(i,1) || round(xprime(y,x)) == f_y(i,2)) ...
+                        && round(yprime(y,x)) == round(y_domain(i))
+                    plot(x,y,'r.');
+                    break
+                 end
+            end
         end
     end
+     [r,c] = ind2sub(size(vessels),points);
+     plot(c,r,'mx')
+end
+ 
+
+%get x,y coordinates along raphe line
+[y_raphe,x_raphe] = find(round(yprime)==0);
+if debug == 2
+    plot(x_raphe,y_raphe,'b-');
     hold off
 end
+
 
 %create vessel density map
 density_map = plot_vdensity(vessels);
@@ -121,19 +123,13 @@ function J = parabola_criterion(inits)
     if B == 0
         xprime = x;
         yprime = y;
-        
-        xcprime = xc;
-        ycprime = yc;
     else
         %get x and y values in new rotated coordinate system
-        xprime = (x*cosd(B)+y*sind(B))/(cosd(B)^2+sind(B)^2);
+        xprime = (x*cosd(B)+y*sind(B))/(sind(B)^2+cosd(B)^2);
         yprime = (xprime*cosd(B)-x)/sind(B);
-        
-        xcprime = (xc*cosd(B)+yc*sind(B))/(cosd(B)^2+sind(B)^2);
-        ycprime = (xcprime*cosd(B)-xc)/sind(B);
     end
     
-    J = a*((xprime-xcprime)*sind(B)+(yprime-ycprime)*cosd(B)).^2 ...
-            - ((xprime-xcprime)*cosd(B)-(yprime-ycprime)*sind(B));    
+    J = a*(xprime*sind(B)+yprime*cosd(B)).^2 ...
+            - abs(xprime*cosd(B)-yprime*sind(B));    
 end
 end
